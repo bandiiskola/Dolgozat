e9f96f61fccc74892b9eeb9db46d5d8f10c65868
bandiiskola <nandor.bere@gmail.com>
2023-09-20 10:27:58 +0200

    TarolasKesz

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,28 @@
-var tomb=[]
+var tomb = []
 
-function tarol()
-{
-    if(document.getElementById.value==""||document.getElementById("type")==null)
-    {
+function tarol() {
+    var x = ""
+    if (document.getElementById("rendszam").value == "") {
         alert("Töltsd ki az adatokat!")
-    }
-    else
-    {
-        var obj={
+    } else {
+        if (document.getElementById("otajto").checked) {
+            x = "otajto"
+        } else {
+            x = "haromajto"
+        }
+        var obj = {
             rendszam: document.getElementById("rendszam").value,
             tipus: document.getElementById("type").value,
-    
+            elsotulaj: document.getElementById("elsotulaj").checked,
+            ajtokszama: x
+
         }
+        tomb.push(obj)
+        document.getElementById("rendszam").value = ""
+        document.getElementById("type").value = "benzin"
+        document.getElementById("elsotulaj").checked = false
+        document.getElementById("otajto").checked = true
+        document.getElementById("rendszam").focus()
     }
+    console.log(tomb)
 }
