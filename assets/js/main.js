661006172a47b8ed867bf4ee74e7233835b9c574
bandiiskola <nandor.bere@gmail.com>
2023-09-20 10:12:57 +0200

    Tarolas

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+var tomb=[]
+
+function tarol()
+{
+    if(document.getElementById.value==""||document.getElementById("type")==null)
+    {
+        alert("Töltsd ki az adatokat!")
+    }
+    else
+    {
+        var obj={
+            rendszam: document.getElementById("rendszam").value,
+            tipus: document.getElementById("type").value,
+    
+        }
+    }
+}
